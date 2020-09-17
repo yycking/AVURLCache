17cdeb4020c00973ee8c91629f0c1a017e6f45ca
Wayne Yeh <wayne.yeh@eten.com.tw>
2020-09-17 13:23:27 +0800

    Initial Commit

---
+++ AVURLCache macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  AVURLCache macOS
+//
+//  Created by Wayne Yeh on 2020/9/17.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
